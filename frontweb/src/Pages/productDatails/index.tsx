8e13f92f92b16d21cd48dadf395b2d89e9600d25
MaiconRomario <maiconromario389@hotmail.com>
2025-09-06 16:57:37 -0300

    ProductDetails route

---
+++ frontweb/src/Pages/productDatails/index.tsx
@@ -0,0 +1,7 @@
+import './style.css';
+
+const ProductDatails = () => {
+  return <h1>Detalhe do produto</h1>;
+};
+
+export default ProductDatails;
--- frontweb/src/routes.tsx
+++ frontweb/src/routes.tsx
@@ -2,6 +2,7 @@ import Navbar from 'components/Navbar';
 import Admin from 'Pages/Admin';
 import Catalog from 'Pages/Catalog';
 import Home from 'Pages/Home';
+import ProductDatails from 'Pages/productDatails';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const AppRoutes = () => (
@@ -10,6 +11,7 @@ const AppRoutes = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Catalog />} />
+      <Route path="/products/:productId" element={<ProductDatails />} />
       <Route path="/admin" element={<Admin />} />
     </Routes>
   </BrowserRouter>
